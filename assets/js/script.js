cedffafdfbcf1c2619df43f54c7353f189e75ad8
joaozinsh <joaogabriel241201@gmail.com>
2021-08-22 16:55:37 -0300

    Criação da comida

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,11 @@ snake[0] = {
     y: 8 * box
 }
 
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
+
 let direction = "right";
 
 function criarBG() {
@@ -20,6 +25,11 @@ function criarCobrinha() {
         context.fillStyle = "green";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function criarComida() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', movimento);
@@ -39,6 +49,7 @@ function iniciarJogo() {
 
     criarBG();
     criarCobrinha();
+    criarComida();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
